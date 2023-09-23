9fa5056370be4476a48660f54ccccbd357d30c0b
RondaKuraI <johnronda0527@gmail.com>
2023-09-23 18:13:54 +0800

    initial code

---
+++ app/Views/product/index.php
@@ -0,0 +1,14 @@
+<?= $this->include('layouts/bootstrap')?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-5">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Products Listing</h>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
